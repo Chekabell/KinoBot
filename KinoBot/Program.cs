b18953dd5b5fdc2240c8c7fd11b8915f180e2464
Chekabell <nikita.toporischev@mail.ru>
2024-11-16 13:12:13 +0500

    Done. And add .gitignore

---
+++ KinoBot/Program.cs
@@ -0,0 +1,189 @@
+﻿using RestSharp;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Newtonsoft.Json;
+
+namespace KinoBot{
+    
+    class Programm
+    {
+        private static ReplyKeyboardMarkup? _replyMarkup;
+        private static Dictionary<long, bool[]> _isProgress;
+        private static readonly string?[] Tokens = new string?[2];
+
+        private static void AddHead(RestRequest? request)
+        {
+            if (request != null)
+            {
+                request.AddHeader("accept", "application/json");
+                request.AddHeader("X-API-KEY", Tokens[0]);
+            }
+        }
+
+        static async Task GetTokens()
+        {
+            StreamReader sr = new StreamReader( "../../../tokens.txt");
+            Tokens[0] = await sr.ReadLineAsync();
+            Tokens[1] = await sr.ReadLineAsync();
+            sr.Close();
+        }
+        
+        static async Task Main()
+        {
+            await GetTokens();
+            if (Tokens[1] != null)
+            {
+                var client = new TelegramBotClient(Tokens[1]);
+                _isProgress = new Dictionary<long, bool[]>();
+                _replyMarkup = new ReplyKeyboardMarkup((KeyboardButton[])[
+                    new KeyboardButton("Найти фильм по названию"),
+                    new KeyboardButton("Получить радномный тайтл"),
+                    new KeyboardButton("Найти актёра по имени")
+                ])
+                {
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = true
+                };
+                client.StartReceiving(Update, Error);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if(message != null && !_isProgress.ContainsKey(message.Chat.Id)) _isProgress.Add(message.Chat.Id, [false,false]);
+            if (message?.Text != null)
+            {
+               Console.WriteLine($"{message.Chat.Username} : {message.Text} ");
+               var bools = _isProgress[message.Chat.Id];
+               
+                if (message.Text.ToLower().Contains("/start"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выбери пункт меню", replyMarkup: _replyMarkup, cancellationToken: token);
+                }
+                
+                if (bools[0] == false && bools[1] == false && message.Text == "Найти фильм по названию")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Напиши название фильма", cancellationToken: token);
+                    if (_replyMarkup != null) _replyMarkup.IsPersistent = false;
+                    bools[0] = true;
+                }
+                else if (bools[0])
+                {
+                    var client = new RestClient(new RestClientOptions("https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=1&query=" + message.Text));
+                    var request = new RestRequest("");
+                    AddHead(request);
+                    
+                    var response = await client.GetAsync(request, cancellationToken: token);
+                    
+                    Console.WriteLine(response.Content);
+                    if (response.Content != null)
+                    {
+                        var root = JsonConvert.DeserializeObject<RootMovie>(response.Content);
+                    
+                        if (root?.total != 0)
+                        {
+                            var url = root?.docs[0].poster?.previewUrl;
+                            var text = "";
+                            if (root?.docs[0].name != "null") text += "Название: " + root?.docs[0].name;
+                            if (root?.docs[0].rating?.kp != null) text += "\n\nРейтинг КиноПоиска: " + root?.docs[0].rating?.kp;
+                            if (root?.docs[0].description != "null") text += "\n\nОписание: " + root?.docs[0].description;
+                        
+                            if (!string.IsNullOrEmpty(url))
+                                await botClient.SendPhotoAsync(message.Chat.Id, new InputFileUrl(url), caption: text, cancellationToken: token);
+                            else 
+                                await botClient.SendTextMessageAsync(message.Chat.Id, text, cancellationToken: token);
+
+                        }
+                        else
+                            await botClient.SendTextMessageAsync(message.Chat.Id,
+                                "По вашему запросу ничего не найдено. Попробуйте ещё раз", cancellationToken: token);
+                    }
+
+                    bools[0] = false;
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите пункт меню", replyMarkup: _replyMarkup, cancellationToken: token);
+                }
+                else if (bools[0] == false && bools[1] == false && message.Text == "Найти актёра по имени")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Напиши имя актёра", cancellationToken: token);
+                    
+                    bools[1] = true;
+                }
+                else if (bools[1])
+                {
+                    var client = new RestClient(new RestClientOptions("https://api.kinopoisk.dev/v1.4/person/search?page=1&limit=1&query=" + message.Text));
+                    var request = new RestRequest("");
+                    AddHead(request);
+                    
+                    var response = await client.GetAsync(request, cancellationToken: token);
+                    
+                    Console.WriteLine(response.Content);
+                    if (response.Content != null)
+                    {
+                        var root = JsonConvert.DeserializeObject<RootPerson>(response.Content);
+                    
+                        if (root?.total != 0)
+                        {
+                            var url = root?.docs[0].photo;
+                            var text = "";
+                            if (root?.docs[0].name != "") text += "Имя: " + root?.docs[0].name;
+                            if (root?.docs[0].sex != "") text += "\n\nПол: " + root?.docs[0].sex;
+                            if (root?.docs[0].age != 0) text += "\n\nВозраст: " + root?.docs[0].age;
+                            if (root?.docs[0].growth != 0) text += "\n\nРост: " + root?.docs[0].growth;
+                        
+                            if (!string.IsNullOrEmpty(url))
+                                await botClient.SendPhotoAsync(message.Chat.Id, new InputFileUrl(url), caption: text, cancellationToken: token);
+                            else 
+                                await botClient.SendTextMessageAsync(message.Chat.Id, "Без фотки отправлять не очень, но такой человек есть.", cancellationToken: token);
+                        }
+                        else
+                            await botClient.SendTextMessageAsync(message.Chat.Id,
+                                "По вашему запросу ничего не найдено. Попробуйте ещё раз", cancellationToken: token);
+                    }
+
+                    bools[1] = false;
+                    
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите пункт меню", replyMarkup: _replyMarkup, cancellationToken: token);
+                }
+                else if (bools[0] == false && bools[1] == false && message.Text == "Получить радномный тайтл")
+                {
+                    var client = new RestClient(new RestClientOptions("https://api.kinopoisk.dev/v1.4/movie/random?lists=top250"));
+                    var request = new RestRequest("");
+                    AddHead(request);
+                    
+                    var response = await client.GetAsync(request, cancellationToken: token);
+                    
+                    Console.WriteLine(response.Content);
+                    if (response.Content != null)
+                    {
+                        var root = JsonConvert.DeserializeObject<RootRandMovie>(response.Content);
+                    
+                        var url = root?.poster.previewUrl;
+                        var text = "";
+                        if (root?.name != "null") text += "Название: " + root?.name;
+                        if (root?.rating.kp != null) text += "\n\nРейтинг КиноПоиска: " + root?.rating.kp;
+                        if (root?.description != "null") text += "\n\nОписание: " + root?.description;
+                        
+                        if (!string.IsNullOrEmpty(url))
+                            await botClient.SendPhotoAsync(message.Chat.Id, new InputFileUrl(url), caption: text, cancellationToken: token);
+                        else 
+                            await botClient.SendTextMessageAsync(message.Chat.Id, text, cancellationToken: token);
+                    }
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Выберите пункт меню", replyMarkup: _replyMarkup, cancellationToken: token);
+                }
+            }
+        }
+        
+        private static Task Error(ITelegramBotClient botClient, Exception e, CancellationToken arg3)
+        {
+            Console.WriteLine(e.Message);
+            return Task.CompletedTask;
+        }
+
+        
+    }
+}
---
+++ KinoBot/Response.cs
@@ -0,0 +1,306 @@
+﻿namespace KinoBot;
+
+
+//Response Movie
+public class RootMovie
+{
+    public List<docs> docs { get; set; }
+    public int total { get; set; }
+    public int limit { get; set; }
+    public int page { get; set; }
+    public int pages { get; set; }
+}
+public class genres
+{
+    public string name { get; set; }
+}
+
+public class countries
+{
+    public string name { get; set; }
+}
+
+public class externalId
+{
+    public string imdb { get; set; }
+    public int tmdb { get; set; }
+    public string kpHD { get; set; }
+}
+
+public class logo
+{
+    public string? url { get; set; }
+}
+
+public class poster
+{
+    public string? url { get; set; }
+    public string? previewUrl { get; set; }
+}
+
+public class backdrop
+{
+    public string? url { get; set; }
+    public string? previewUrl { get; set; }
+}
+
+public class rating
+{
+    public float? kp { get; set; }
+    public float? imdb { get; set; }
+    public float? filmCritics { get; set; }
+    public float? russianFilmCritics { get; set; }
+    public object? await { get; set; }
+}
+
+public class votes
+{
+    public int? kp { get; set; }
+    public int? imdb { get; set; }
+    public int? filmCritics { get; set; }
+    public int? russianFilmCritics { get; set; }
+    public int? await { get; set; }
+}
+
+public class docs
+{
+    public List<object> internalNames { get; set; }
+    public string? name { get; set; }
+    public string? alternativeName { get; set; }
+    public string? enName { get; set; }
+    public int? year { get; set; }
+    public List<genres> genres { get; set; }
+    public List<countries> countries { get; set; }
+    public List<object> releaseYears { get; set; }
+    public int? id { get; set; }
+    public externalId? externalId { get; set; }
+    public List<object> names { get; set; }
+    public string? type { get; set; }
+    public string? description { get; set; }
+    public string? shortDescription { get; set; }
+    public logo? logo { get; set; }
+    public poster? poster { get; set; }
+    public backdrop? backdrop { get; set; }
+    public rating? rating { get; set; }
+    public votes? votes { get; set; }
+    public int? movieLength { get; set; }
+    public float? internalRating { get; set; }
+    public int? internalVotes { get; set; }
+    public bool? isSeries { get; set; }
+    public bool? ticketsOnSale { get; set; }
+    public object? totalSeriesLength { get; set; }
+    public object? seriesLength { get; set; }
+    public string? ratingMpaa { get; set; }
+    public int? ageRating { get; set; }
+    public object? top10 { get; set; }
+    public object? top250 { get; set; }
+    public int? typeNumber { get; set; }
+    public object? status { get; set; }
+}  
+
+
+//Response person
+public class Doc
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string enName { get; set; }
+    public string photo { get; set; }
+    public string sex { get; set; }
+    public int growth { get; set; }
+    public DateTime? birthday { get; set; }
+    public string death { get; set; }
+    public int age { get; set; }
+}
+
+public class RootPerson
+{
+    public List<Doc> docs { get; set; }
+    public int total { get; set; }
+    public int limit { get; set; }
+    public int page { get; set; }
+    public int pages { get; set; }
+}
+
+// RootRandMovie
+    public class Audience
+    {
+        public int count { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Backdrop
+    {
+        public string url { get; set; }
+        public string previewUrl { get; set; }
+    }
+
+    public class Country
+    {
+        public string name { get; set; }
+    }
+
+    public class ExternalId
+    {
+        public string kpHD { get; set; }
+        public string imdb { get; set; }
+        public int tmdb { get; set; }
+    }
+
+    public class Fees
+    {
+        public Russia russia { get; set; }
+        public Usa usa { get; set; }
+        public World world { get; set; }
+    }
+
+    public class Genre
+    {
+        public string name { get; set; }
+    }
+
+    public class Item
+    {
+        public string name { get; set; }
+        public Logo logo { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Logo
+    {
+        public string url { get; set; }
+        public string previewUrl { get; set; }
+    }
+
+    public class Name
+    {
+        public string name { get; set; }
+        public string language { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Person
+    {
+        public int id { get; set; }
+        public string photo { get; set; }
+        public string name { get; set; }
+        public string enName { get; set; }
+        public string description { get; set; }
+        public string profession { get; set; }
+        public string enProfession { get; set; }
+    }
+
+    public class Poster
+    {
+        public string url { get; set; }
+        public string previewUrl { get; set; }
+    }
+
+    public class Premiere
+    {
+        public object country { get; set; }
+        public object cinema { get; set; }
+        public object bluray { get; set; }
+        public object dvd { get; set; }
+        public DateTime? digital { get; set; }
+        public DateTime? russia { get; set; }
+        public DateTime? world { get; set; }
+    }
+
+    public class Rating
+    {
+        public double kp { get; set; }
+        public double imdb { get; set; }
+        public double filmCritics { get; set; }
+        public double russianFilmCritics { get; set; }
+        public object await { get; set; }
+    }
+
+public class RootRandMovie
+    {
+        public int id { get; set; }
+        public ExternalId externalId { get; set; }
+        public string name { get; set; }
+        public string alternativeName { get; set; }
+        public string enName { get; set; }
+        public List<Name> names { get; set; }
+        public string type { get; set; }
+        public int typeNumber { get; set; }
+        public int year { get; set; }
+        public string description { get; set; }
+        public string shortDescription { get; set; }
+        public string slogan { get; set; }
+        public object status { get; set; }
+        public Rating rating { get; set; }
+        public Votes votes { get; set; }
+        public int? movieLength { get; set; }
+        public object totalSeriesLength { get; set; }
+        public object seriesLength { get; set; }
+        public object ratingMpaa { get; set; }
+        public int? ageRating { get; set; }
+        public Poster poster { get; set; }
+        public Backdrop backdrop { get; set; }
+        public List<Genre> genres { get; set; }
+        public List<Country> countries { get; set; }
+        public List<Person> persons { get; set; }
+        public Premiere premiere { get; set; }
+        public Watchability watchability { get; set; }
+        public object top10 { get; set; }
+        public int? top250 { get; set; }
+        public bool isSeries { get; set; }
+        public List<Audience> audience { get; set; }
+        public bool ticketsOnSale { get; set; }
+        public List<string> lists { get; set; }
+        public object networks { get; set; }
+        public DateTime? createdAt { get; set; }
+        public DateTime? updatedAt { get; set; }
+        public Fees fees { get; set; }
+        public Videos videos { get; set; }
+        public Logo logo { get; set; }
+    }
+
+    public class Russia
+    {
+        public int value { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Trailer
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string site { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Usa
+    {
+        public int value { get; set; }
+        public string currency { get; set; }
+    }
+
+    public class Videos
+    {
+        public List<Trailer> trailers { get; set; }
+    }
+
+    public class Votes
+    {
+        public float? kp { get; set; }
+        public float? imdb { get; set; }
+        public float? filmCritics { get; set; }
+        public float? russianFilmCritics { get; set; }
+        public int await { get; set; }
+    }
+
+    public class Watchability
+    {
+        public List<Item> items { get; set; }
+    }
+
+    public class World
+    {
+        public int value { get; set; }
+        public string currency { get; set; }
+    }
+
